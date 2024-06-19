be5ac4ffed50878adaf06ff9edab7a6e693237c6
Judith <judith.sanmartin.s@gmail.com>
2024-06-18 22:04:26 -0400

    sigo intentndo

--- index.js
+++ index.js
@@ -1,93 +1,61 @@
-import 'dotenv/config'
-import axios from 'axios'
-import express from 'express'
-import Jimp from 'jimp'
-import _ from 'lodash'
-import moment from 'moment'
-import { nanoid } from 'nanoid'
-import nodemailer from 'nodemailer'
-
+import express from 'express';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment';
+import _ from 'lodash';
+import chalk from 'chalk';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express()
-app.use(express.static(__dirname + '/public'))
-
-const users = []
-
-app.get('/', async (req, res) => {
-
-    const response = await axios.get('https://randomuser.me/api/')
-    const gender = response.data.results[0].gender
-    const first = response.data.results[0].name.first
-    const last = response.data.results[0].name.last
-
-    const user = {
-        gender,
-        first,
-        last,
-        id: nanoid(),
-        timestamp: moment().format('LLL')
+async function fetchRandomUserData() {
+    try {
+        const response = await axios.get('https://randomuser.me/api/');
+        return response.data.results[0];
+    } catch (error) {
+        console.error('Error al obtener datos de la API Random User:', error);
+        throw error;
     }
+}
 
-    users.push(user)
-    const newUsers = _.partition(users, (item) => item.gender === 'female')
-    const females = newUsers[0];
-    const males = newUsers[1];
+function generateUniqueId() {
+    return uuidv4();
+}
 
-    console.log(chalk.bgWhite.blue(JSON.stringify({ females, males }, null, 2)));
+function addTimestamp(user) {
+    user.timestamp = moment().format('MMMM Do YYYY, h:mm:ss a');
+    return user;
+}
 
-    res.json({
-        females, male
-    })
-})
+function splitUsersByGender(users) {
+    return _.partition(users, (user) => user.gender === 'female');
+}
 
+function logUsers(users) {
+    console.log(chalk.bgWhite.blue(JSON.stringify({ females: users[0], males: users[1] }, null, 2)));
+}
 
-app.get('/image', async (req, res) => {
-    const image = await Jimp.read('https://picsum.photos/1000')
-    const buffer = await image
-        .resize(500, 500)
-        .grayscale()
-        .quality(60)
-        .getBufferAsync(Jimp.MIME_JPEG)
+app.get('/register', async (req, res) => {
+    try {
 
-    const dirname = __dirname + `/public/img/image-${nanoid()}.jpeg`
-    await image.writeAsync(dirname)
+        const randomUser = await fetchRandomUserData();
 
-    res.set("Content-Type", "image/jpeg")
-    return res.send(buffer)
-})
+        randomUser.id = generateUniqueId();
 
-app.get('/mail', async (req, res) => {
+        addTimestamp(randomUser);
 
-    const transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+        res.json(randomUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al registrar usuario' });
+    }
+});
 
-    const info = await transport.sendMail({
-        from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', 
-        to: "bar@example.com, baz@example.com",
-        subject: "Hello ✔", 
-        text: "Hello world?", 
-        html: "<b>Hello world?</b>",
-    });
-
-    console.log("Message sent: %s", info.messageId);
-
-    return res.json({ ok: true })
-
-})
-
-
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Example app listening on PORT ${PORT}`)
-    console.log(process.pid)
-})
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+export default app;
